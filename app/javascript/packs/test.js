b6af7c1d653418435f3ea18039ead679a4cb7470
AYAITABASHI <119820092+AYA-ITABASHI@users.noreply.github.com>
2023-05-29 23:42:50 +0000

    [Add]stop

--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -1,26 +1,34 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded")
+document.addEventListener("DOMContentLoaded",() => {
 
-  const timer = document.getElementById("time");
+  const timer = document.getElementById("timer");
   const start = document.getElementById("start") ;
   const stop = document.getElementById("stop");
   const reset = document.getElementById("reset");
 
-let startTime;
+  let startTime;
+  let timeOutid;
 
-function countUp(){
- console.log(Date.now()-startTime);
- const d = new Date(Date.now()-startTime);
- const m =String(d.getMinutes()).padStart(2, "0");
+  function countUp(){
 
- setTimeout(() => {
+  const d = new Date(Date.now()-startTime);
+  const h =String(d.getUTCHours()).padStart(2, "0");
+  const m =String(d.getMinutes()).padStart(2, "0");
+  const s =String(d.getSeconds()).padStart(2, "0");
+  timer.textContent = `${h}:${m}:${s}`;
+
+   timeOutid=setTimeout(() => {
    countUp();
-  }, 10);
- }
+   }, 10);
+   }
 
- start.addEventListener('click', () => {
-  startTime =Date.now();
-  countUp();
+  start.addEventListener('click', () => {
+   startTime = Date.now();
+   countUp();
+  });
 
- });
+  stop.addEventListener('click', () => {
+   clearTimeout(timeOutid);
+  });
+});
