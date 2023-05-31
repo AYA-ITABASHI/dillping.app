f9f438d94cb520f08b571587ec80efb2026a28a3
AYAITABASHI <119820092+AYA-ITABASHI@users.noreply.github.com>
2023-05-31 23:53:20 +0000

    [Add]stopWatch

--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -9,10 +9,11 @@ document.addEventListener("DOMContentLoaded",() => {
 
   let startTime;
   let timeOutid;
+  let elapsedTime = 0;
 
   function countUp(){
 
-  const d = new Date(Date.now()-startTime);
+  const d = new Date(Date.now()-startTime + elapsedTime);
   const h =String(d.getUTCHours()).padStart(2, "0");
   const m =String(d.getMinutes()).padStart(2, "0");
   const s =String(d.getSeconds()).padStart(2, "0");
@@ -23,12 +24,28 @@ document.addEventListener("DOMContentLoaded",() => {
    }, 10);
    }
 
+  function setButtonsInitial(){
+   start.diabled = false
+   stop.disabled = true
+   reset.disabled = true
+  }
+
+  setButtonsInitial();
+
   start.addEventListener('click', () => {
+
    startTime = Date.now();
    countUp();
   });
 
   stop.addEventListener('click', () => {
    clearTimeout(timeOutid);
+   elapsedTime += Date.now()- startTime
   });
+
+  reset.addEventListener('click', () => {
+   timer.textContent = "00:00:00"
+   elapsedTime = 0;
+  });
+
 });
